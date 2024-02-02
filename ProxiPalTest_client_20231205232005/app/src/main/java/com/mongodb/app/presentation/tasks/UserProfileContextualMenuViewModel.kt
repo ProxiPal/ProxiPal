aca2f97b8ae2efb454c49998cb49ec5d3ac3efde
kevink02 <kevinkubota0@outlook.com>
2024-02-01 20:44:38 -0800

    Create UserProfileContextualMenuViewModel.kt
    Copied format of ItemContextualMenuViewModel class for UserProfiles instead of Items

---
+++ ProxiPalTest_client_20231205232005/app/src/main/java/com/mongodb/app/presentation/tasks/UserProfileContextualMenuViewModel.kt
@@ -0,0 +1,45 @@
+package com.mongodb.app.presentation.tasks
+// TODO Might need to move this class to a new package to avoid confusion being in the "tasks" package
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.mongodb.app.data.SyncRepository
+import com.mongodb.app.domain.UserProfile
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.launch
+
+class UserProfileContextualMenuViewModel constructor(
+    private val repository: SyncRepository,
+    private val userProfileViewModel: UserProfileViewModel
+) : ViewModel() {
+
+    private val _visible: MutableState<Boolean> = mutableStateOf(false)
+    val visible: State<Boolean>
+        get() = _visible
+
+    fun open() {
+        _visible.value = true
+    }
+
+    fun close() {
+        _visible.value = false
+    }
+
+    fun deleteUserProfile(userProfile: UserProfile) {
+        CoroutineScope(Dispatchers.IO).launch {
+            if (repository.isUserProfileMine(userProfile)) {
+                runCatching {
+                    repository.deleteUserProfile(userProfile)
+                }
+            } else {
+                userProfileViewModel.showPermissionsMessage()
+            }
+        }
+        close()
+    }
+}
