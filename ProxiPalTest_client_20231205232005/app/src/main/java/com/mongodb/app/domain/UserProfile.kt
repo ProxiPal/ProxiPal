3cb87c4f124088c9865eca20320f5867b8d98188
kevink02 <kevinkubota0@outlook.com>
2024-02-01 20:03:28 -0800

    Create UserProfile.kt
    Copied format of Item class

---
+++ ProxiPalTest_client_20231205232005/app/src/main/java/com/mongodb/app/domain/UserProfile.kt
@@ -0,0 +1,31 @@
+package com.mongodb.app.domain
+
+import org.mongodb.kbson.ObjectId
+import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.PrimaryKey
+
+class UserProfile() : RealmObject {
+    @PrimaryKey
+    var _id: ObjectId = ObjectId()
+    var firstName: String = ""
+    var lastName: String = ""
+    var biography: String = ""
+
+    override fun equals(other: Any?): Boolean {
+        if (other == null) return false
+        if (other !is UserProfile) return false
+        if (this._id != other._id) return false
+        if (this.firstName != other.firstName) return false
+        if (this.lastName != other.lastName) return false
+        if (this.biography != other.biography) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = _id.hashCode()
+        result = 31 * result + firstName.hashCode()
+        result = 31 * result + lastName.hashCode()
+        result = 31 * result + biography.hashCode()
+        return result
+    }
+}
