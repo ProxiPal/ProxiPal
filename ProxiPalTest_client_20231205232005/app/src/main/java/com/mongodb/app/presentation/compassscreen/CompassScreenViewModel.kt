b912cbb0c5ea54d6d1afb60fcc25cfcc3cff724a
kevink02 <kevinkubota0@outlook.com>
2024-02-22 12:34:40 -0800

    Added compass screen and view model classes

---
+++ ProxiPalTest_client_20231205232005/app/src/main/java/com/mongodb/app/presentation/compassscreen/CompassScreenViewModel.kt
@@ -0,0 +1,151 @@
+package com.mongodb.app.presentation.compassscreen
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.mongodb.app.data.UserLocation
+import com.mongodb.app.ui.compassscreen.CompassUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class CompassViewModel : ViewModel() {
+    /*
+    ===== Variables =====
+     */
+    private val _userLocations = MutableStateFlow(CompassUiState())
+
+    private val _currentUserLatitude: MutableState<String> = mutableStateOf(
+        "0.0"
+    )
+
+    private val _currentUserLongitude: MutableState<String> = mutableStateOf(
+        "0.0"
+    )
+
+    private val _matchedUserLatitude: MutableState<String> = mutableStateOf(
+        "0.0"
+    )
+
+    private val _matchedUserLongitude: MutableState<String> = mutableStateOf(
+        "0.0"
+    )
+
+    private val _isMeetingWithMatch: MutableState<Boolean> = mutableStateOf(true)
+
+
+    /*
+    ===== Properties =====
+     */
+    val userLocations: StateFlow<CompassUiState> = _userLocations.asStateFlow()
+
+    val currentUserLatitude: State<String>
+        get() = _currentUserLatitude
+
+    val currentUserLongitude: State<String>
+        get() = _currentUserLongitude
+
+    val matchedUserLatitude: State<String>
+        get() = _matchedUserLatitude
+
+    val matchedUserLongitude: State<String>
+        get() = _matchedUserLongitude
+
+    val isMeetingWithMatch: State<Boolean>
+        get() = _isMeetingWithMatch
+
+
+    init{
+        // Start the compass screen with the user currently meeting up with their match
+        _isMeetingWithMatch.value = true
+    }
+
+
+    /*
+    ===== Functions =====
+     */
+    private fun isValidLatitude(latitude: Double): Boolean{
+        return latitude in -90.0..90.0
+    }
+
+    private fun isValidLongitude(longitude: Double): Boolean{
+        return longitude in -180.0..180.0
+    }
+
+    /**
+     * Updates the current user's latitude
+     */
+    fun updateCurrentUserLatitude(newLatitude: String){
+        _currentUserLatitude.value = newLatitude
+        if (newLatitude.toDoubleOrNull() != null && isValidLatitude(newLatitude.toDouble())){
+            _userLocations.update { currentState ->
+                currentState.copy(
+                    currentUserLocation = UserLocation(
+                        latitude = newLatitude.toDouble(),
+                        longitude = userLocations.value.currentUserLocation.longitude
+                    )
+                )
+            }
+        }
+    }
+
+    /**
+     * Updates the current user's longitude
+     */
+    fun updateCurrentUserLongitude(newLongitude: String){
+        _currentUserLongitude.value = newLongitude
+        if (newLongitude.toDoubleOrNull() != null && isValidLongitude(newLongitude.toDouble())){
+            _userLocations.update { currentState ->
+                currentState.copy(
+                    currentUserLocation = UserLocation(
+                        latitude = userLocations.value.currentUserLocation.latitude,
+                        longitude = newLongitude.toDouble()
+                    )
+                )
+            }
+        }
+    }
+
+    /**
+     * Updates the matched user's latitude
+     */
+    fun updateMatchedUserLatitude(newLatitude: String) {
+        _matchedUserLatitude.value = newLatitude
+        if (newLatitude.toDoubleOrNull() != null && isValidLatitude(newLatitude.toDouble())){
+            _userLocations.update { currentState ->
+                currentState.copy(
+                    matchedUserLocation = UserLocation(
+                        latitude = newLatitude.toDouble(),
+                        longitude = userLocations.value.matchedUserLocation.longitude
+                    )
+                )
+            }
+        }
+    }
+
+    /**
+     * Updates the matched user's longitude
+     */
+    fun updateMatchedUserLongitude(newLongitude: String) {
+        _matchedUserLongitude.value = newLongitude
+        if (newLongitude.toDoubleOrNull() != null && isValidLongitude(newLongitude.toDouble())){
+            _userLocations.update { currentState ->
+                currentState.copy(
+                    matchedUserLocation = UserLocation(
+                        latitude = userLocations.value.matchedUserLocation.latitude,
+                        longitude = newLongitude.toDouble()
+                    )
+                )
+            }
+        }
+    }
+
+    /**
+     * Toggles whether the current user is meeting up with their matched user
+     */
+    fun toggleMeetingWithMatch(){
+        _isMeetingWithMatch.value = !_isMeetingWithMatch.value
+    }
+}
---
+++ ProxiPalTest_client_20231205232005/app/src/main/java/com/mongodb/app/ui/compassscreen/CompassScreen.kt
@@ -0,0 +1,435 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.mongodb.app.ui.compassscreen
+
+import android.util.Log
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.mongodb.app.R
+import com.mongodb.app.data.KM_PER_ONE_LATITUDE_DIFF
+import com.mongodb.app.data.KM_PER_ONE_LONGITUDE_DIFF
+import com.mongodb.app.presentation.compassscreen.CompassViewModel
+import com.mongodb.app.ui.theme.MyApplicationTheme
+import kotlin.math.atan2
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+
+/*
+===== Functions =====
+ */
+/**
+ * Displays the entire connection with others screen
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CompassScreenLayout(
+    modifier: Modifier = Modifier
+) {
+    // Temporary way of accessing view model
+    // TODO Update when integrating into the main code
+    val compassViewModel: CompassViewModel = viewModel()
+
+    Scaffold(
+        topBar = {
+            CompassScreenTopBar(
+
+            )
+        },
+        modifier = modifier
+        // Pad the body of content so it does not get cut off by the scaffold top bar
+    ) { innerPadding ->
+        CompassScreenBodyContent(
+            compassViewModel = compassViewModel,
+            modifier = Modifier
+                .padding(innerPadding)
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CompassScreenTopBar(
+    modifier: Modifier = Modifier
+) {
+    // Back button is automatically handled by the navigation code (?)
+    // ... so it's not programmed here
+    CenterAlignedTopAppBar(
+        title = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    text = stringResource(
+                        id = R.string.compass_screen_top_bar_header
+                    ),
+                    style = MaterialTheme.typography.displayMedium
+                )
+            }
+        },
+        modifier = modifier
+    )
+}
+
+/**
+ * Displays the main body of content
+ */
+@Composable
+fun CompassScreenBodyContent(
+    compassViewModel: CompassViewModel,
+    modifier: Modifier = Modifier
+) {
+    val compassState = compassViewModel.userLocations.collectAsState()
+    val bearing = calculateBearingBetweenPoints(
+        startLatitude = compassState.value.currentUserLocation.latitude,
+        startLongitude = compassState.value.currentUserLocation.longitude,
+        endLatitude = compassState.value.matchedUserLocation.latitude,
+        endLongitude = compassState.value.matchedUserLocation.longitude
+    )
+    val distance = calculateDistanceBetweenPoints(
+        startLatitude = compassState.value.currentUserLocation.latitude,
+        startLongitude = compassState.value.currentUserLocation.longitude,
+        endLatitude = compassState.value.matchedUserLocation.latitude,
+        endLongitude = compassState.value.matchedUserLocation.longitude
+    )
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        CompassScreenCompassVisual(
+            compassViewModel = compassViewModel
+        )
+        CompassScreenMeasurementText(
+            measurementText = R.string.compass_screen_bearing_message,
+            measurement = bearing
+        )
+        CompassScreenMeasurementText(
+            measurementText = R.string.compass_screen_distance_message,
+            measurement = distance
+        )
+        if (compassViewModel.isMeetingWithMatch.value) {
+            CompassScreenCancelButton(
+                compassViewModel = compassViewModel
+            )
+        }
+        TempCompassScreenLocationUpdating(
+            compassViewModel = compassViewModel
+        )
+        TempCompassScreenLogMessages(
+            compassViewModel = compassViewModel
+        )
+    }
+}
+
+/**
+ * Displays the compass that points matches toward each other
+ */
+@Composable
+fun CompassScreenCompassVisual(
+    compassViewModel: CompassViewModel,
+    modifier: Modifier = Modifier
+) {
+    val compassState = compassViewModel.userLocations.collectAsState()
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.tempcompass),
+            contentDescription = "Compass pointing you to your matching user",
+            modifier = Modifier
+                // Image rotation is clockwise
+                .rotate(
+                    degrees = calculateBearingBetweenPoints(
+                        startLatitude = compassState.value.currentUserLocation.latitude,
+                        startLongitude = compassState.value.currentUserLocation.longitude,
+                        endLatitude = compassState.value.matchedUserLocation.latitude,
+                        endLongitude = compassState.value.matchedUserLocation.longitude
+                    ).toFloat()
+                )
+        )
+    }
+}
+
+@Composable
+fun CompassScreenMeasurementText(
+    @StringRes measurementText: Int,
+    measurement: Double,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = stringResource(id = measurementText, measurement)
+        )
+    }
+}
+
+/**
+ * Calculates the bearing angle, in degrees, between two points
+ * (Bearing angle should be 0 degrees in the +y direction and increase clockwise)
+ */
+private fun calculateBearingBetweenPoints(
+    startLatitude: Double,
+    startLongitude: Double,
+    endLatitude: Double,
+    endLongitude: Double
+): Double {
+    val deltaLatitude = endLatitude - startLatitude
+    val deltaLongitude = endLongitude - startLongitude
+    // The user is at the same location as their match
+    if (deltaLatitude == 0.0 && deltaLongitude == 0.0) {
+        return 0.0
+    }
+    var theta = atan2(deltaLatitude, deltaLongitude)
+    // Convert the angle to degrees
+    theta = Math.toDegrees(theta)
+    // Subtract 90 to make 0 degrees point north instead of east
+    theta -= 90
+    // Make the theta non-negative
+    if (theta < 0) {
+        theta += 360
+    }
+    // Reverse the direction the bearing increases, from counter-clockwise to clockwise
+    theta = 360 - theta
+    // Convert 360 degrees to 0
+    // Theta should never be above 360, but >= check is used anyway
+    if (theta >= 360) {
+        theta -= 360
+    }
+    return theta
+}
+
+/**
+ * Calculates the distance, in km, between two points
+ */
+private fun calculateDistanceBetweenPoints(
+    startLatitude: Double,
+    startLongitude: Double,
+    endLatitude: Double,
+    endLongitude: Double
+): Double {
+    // Using the distance formula
+    // Make sure to take into account the actual distance between points
+    val deltaLatitude = (endLatitude - startLatitude) * KM_PER_ONE_LATITUDE_DIFF
+    val deltaLongitude = (endLongitude - startLongitude) * KM_PER_ONE_LONGITUDE_DIFF
+    return sqrt(deltaLatitude.pow(2) + deltaLongitude.pow(2))
+}
+
+// TODO Need to make sure this cancels the matching process for both users
+@Composable
+fun CompassScreenCancelButton(
+    compassViewModel: CompassViewModel,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        Button(
+            onClick = {
+                compassViewModel.toggleMeetingWithMatch()
+                Log.i(
+                    "tempTag",
+                    "User has canceled meeting with match"
+                )
+            }
+        ) {
+            Text(
+                text = stringResource(
+                    id = R.string.compass_screen_cancel_message
+                )
+            )
+        }
+    }
+}
+
+// region TemporaryUI
+/**
+ * Displays temporary text fields for inputting and changing matching users' locations
+ */
+@Composable
+fun TempCompassScreenLocationUpdating(
+    compassViewModel: CompassViewModel,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = "Current location:",
+                modifier = Modifier
+                    .weight(0.33f)
+            )
+            TempCompassScreenLocationUpdater(
+                userLocationParameterValue = compassViewModel.currentUserLatitude.value,
+                updateUserLocationParameter = { compassViewModel.updateCurrentUserLatitude(it) },
+                modifier = Modifier
+                    .weight(0.33f)
+            )
+            TempCompassScreenLocationUpdater(
+                userLocationParameterValue = compassViewModel.currentUserLongitude.value,
+                updateUserLocationParameter = { compassViewModel.updateCurrentUserLongitude(it) },
+                modifier = Modifier
+                    .weight(0.33f)
+            )
+        }
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = "Match location:",
+                modifier = Modifier
+                    .weight(0.33f)
+            )
+            TempCompassScreenLocationUpdater(
+                userLocationParameterValue = compassViewModel.matchedUserLatitude.value,
+                updateUserLocationParameter = { compassViewModel.updateMatchedUserLatitude(it) },
+                modifier = Modifier
+                    .weight(0.33f)
+            )
+            TempCompassScreenLocationUpdater(
+                userLocationParameterValue = compassViewModel.matchedUserLongitude.value,
+                updateUserLocationParameter = { compassViewModel.updateMatchedUserLongitude(it) },
+                modifier = Modifier
+                    .weight(0.33f)
+            )
+        }
+    }
+}
+
+/**
+ * Displays a temporary text field for updating a user's location
+ */
+@Composable
+fun TempCompassScreenLocationUpdater(
+    userLocationParameterValue: String,
+    updateUserLocationParameter: ((String) -> Unit),
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = userLocationParameterValue,
+        onValueChange = {
+            updateUserLocationParameter(it)
+        },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Decimal,
+            imeAction = ImeAction.Done
+        ),
+        singleLine = true,
+        // Invalid location data will be shown as an error in the text box
+        isError = userLocationParameterValue.toDoubleOrNull() == null,
+        modifier = modifier
+            .padding(all = 4.dp)
+            .width(100.dp)
+    )
+}
+
+@Composable
+fun TempCompassScreenLogMessages(
+    compassViewModel: CompassViewModel,
+    modifier: Modifier = Modifier
+) {
+    val compassState = compassViewModel.userLocations.collectAsState()
+
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        // For debugging purposes only
+        Button(
+            onClick = {
+                Log.i(
+                    "tempTag",
+                    "Mutable state data = " +
+                            "${compassViewModel.currentUserLatitude.value} ; " +
+                            "${compassViewModel.currentUserLongitude.value} ; " +
+                            "${compassViewModel.matchedUserLatitude.value} ; " +
+                            "${compassViewModel.matchedUserLongitude.value} ; "
+                )
+//                Log.i(
+//                    "tempTag",
+//                    "Mutable state bearing = " +
+//                            "${calculateBearingBetweenPoints(
+//                                compassViewModel.currentUserLatitude.value,
+//                                compassViewModel.currentUserLongitude.value,
+//                                compassViewModel.matchedUserLatitude.value,
+//                                compassViewModel.matchedUserLongitude.value
+//                            )}"
+//                )
+                Log.i(
+                    "tempTag",
+                    "UI state data = " +
+                            "${compassState.value.currentUserLocation.latitude} ; " +
+                            "${compassState.value.currentUserLocation.longitude} ; " +
+                            "${compassState.value.matchedUserLocation.latitude} ; " +
+                            "${compassState.value.matchedUserLocation.longitude} ; "
+                )
+                Log.i(
+                    "tempTag",
+                    "UI state bearing = " +
+                            "${
+                                calculateBearingBetweenPoints(
+                                    compassState.value.currentUserLocation.latitude,
+                                    compassState.value.currentUserLocation.longitude,
+                                    compassState.value.matchedUserLocation.latitude,
+                                    compassState.value.matchedUserLocation.longitude
+                                )
+                            }"
+                )
+            },
+            modifier = Modifier
+        ) {
+            Text(
+                text = "Print log message showing currently saved data"
+            )
+        }
+    }
+}
+// endregion TemporaryUI
+
+@Preview(showBackground = true)
+@Composable
+fun CompassScreenLayoutPreview() {
+    MyApplicationTheme {
+        CompassScreenLayout()
+    }
+}
