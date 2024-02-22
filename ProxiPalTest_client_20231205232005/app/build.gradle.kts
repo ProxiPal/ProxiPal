0dbb7aaac30edd51da7cb40bdb4cbe72fa9a4f46
marcopacin1 <marcopacin1@yahoo.com>
2024-02-22 15:03:28 -0800

    Updated gradle scripts and kotlin version in the project, and updated realm version
    Make sure to have the latest version of kotlin downlaoded. Updated realm version because the features I am trying to use require the latest realm version.

--- ProxiPalTest_client_20231205232005/app/build.gradle.kts
+++ ProxiPalTest_client_20231205232005/app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     kotlin("android")
-    id("io.realm.kotlin") version "1.8.0"
+    id("io.realm.kotlin") version "1.13.0"
 }
 
 android {
@@ -22,8 +22,12 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.3.2"
+        kotlinCompilerExtensionVersion = "1.5.10"
     }
+    kotlinOptions {
+        jvmTarget = "1.8" // Set JVM target compatibility
+    }
+    namespace = "com.mongodb.app"
 }
 
 dependencies {
@@ -45,10 +49,10 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
 
     implementation("com.google.android.material:material:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("io.realm.kotlin:library-sync:1.8.0") // DON'T FORGET TO UPDATE VERSION IN PROJECT GRADLE
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    implementation("io.realm.kotlin:library-sync:1.13.0") // DON'T FORGET TO UPDATE VERSION IN PROJECT GRADLE
 
     implementation ("androidx.core:core-ktx:1.12.0")
     implementation ("androidx.compose.ui:ui:1.6.1")
