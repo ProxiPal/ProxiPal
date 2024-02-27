0bc9d02169576d380cc823c1df6ad5d61b0de359
kevink02 <kevinkubota0@outlook.com>
2024-02-26 22:22:38 -0800

    Fixed build errors

--- ProxiPalTest_client_20231205232005/app/build.gradle.kts
+++ ProxiPalTest_client_20231205232005/app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.3.2"
+        kotlinCompilerExtensionVersion = "1.5.2"
     }
 }
 
@@ -61,6 +61,7 @@ dependencies {
     implementation ("androidx.compose.ui:ui-tooling-preview:1.6.1")
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation ("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.core:core-ktx:+")
     testImplementation ("junit:junit:4.13.2")
     androidTestImplementation ("androidx.test.ext:junit:1.1.5")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
