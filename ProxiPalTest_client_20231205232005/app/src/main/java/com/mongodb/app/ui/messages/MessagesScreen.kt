e76f84ed8d561bbdc119e8a2c0574912bec090b7
kevink02 <kevinkubota0@outlook.com>
2024-03-07 17:53:07 -0800

    Started basic messaging screen UI

---
+++ ProxiPalTest_client_20231205232005/app/src/main/java/com/mongodb/app/ui/messages/MessagesScreen.kt
@@ -0,0 +1,263 @@
+package com.mongodb.app.ui.messages
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
+import com.mongodb.app.R
+import com.mongodb.app.TAG
+import com.mongodb.app.data.MockRepository
+import com.mongodb.app.data.RealmSyncRepository
+import com.mongodb.app.presentation.tasks.ToolbarViewModel
+import com.mongodb.app.ui.theme.MyApplicationTheme
+import com.mongodb.app.ui.theme.Purple200
+import kotlinx.coroutines.launch
+
+class MessagesScreen : ComponentActivity(){
+    // region Variables
+    private val repository = RealmSyncRepository { _, _ ->
+        lifecycleScope.launch {
+        }
+    }
+
+    private val toolbarViewModel: ToolbarViewModel by viewModels {
+        ToolbarViewModel.factory(repository, this)
+    }
+    // endregion Variables
+
+
+    // region Functions
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Log.i(
+            TAG(),
+            "MessagesScreen: Start of OnCreate()"
+        )
+
+        setContent {
+            MessagesScreenLayout(
+                toolbarViewModel = toolbarViewModel,
+//                navController =
+            )
+        }
+    }
+    // endregion Functions
+}
+
+
+// region Functions
+/**
+ * Displays the entire messages screen
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MessagesScreenLayout(
+    toolbarViewModel: ToolbarViewModel,
+//    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    Scaffold(
+        topBar = {
+//            TaskAppToolbar(
+//                viewModel = toolbarViewModel,
+////                navController = navController
+//            )
+            MessagesTopBar()
+        },
+        modifier = modifier
+        // Pad the body of content so it does not get cut off by the scaffold top bar
+    ) { innerPadding ->
+        MessagesBodyContent(
+            modifier = Modifier
+                .padding(innerPadding)
+        )
+    }
+}
+
+/**
+ * Displays the top bar for the compass screen
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MessagesTopBar(
+    modifier: Modifier = Modifier
+) {
+    // Back button is automatically handled by the navigation code (?)
+    // ... so it's not programmed here
+    CenterAlignedTopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = stringResource(
+                        id = R.string.app_name
+                    ),
+                    color = Color.White,
+                    style = MaterialTheme.typography.displayMedium
+                )
+            }
+        },
+        colors = TopAppBarDefaults.smallTopAppBarColors(
+            containerColor = Purple200
+        ),
+        modifier = modifier
+    )
+}
+
+/**
+ * The main middle section that displays all the messages between 2 users
+ */
+@Composable
+fun MessagesBodyContent(
+    modifier: Modifier = Modifier
+){
+    Column (
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+    ){
+        SingleMessage(
+            isSenderMe = false,
+            message = "Hello"
+        )
+        SingleMessage(
+            isSenderMe = true,
+            message = "World!"
+        )
+    }
+}
+
+@Composable
+fun SingleMessage(
+    isSenderMe: Boolean,
+    message: String,
+    modifier: Modifier = Modifier
+){
+    val rowArrangement =
+        if (isSenderMe) Arrangement.End else Arrangement.Start
+    val columnAlignment =
+        if (isSenderMe) Alignment.End else Alignment.Start
+    val messageWidthWeight = 0.6f
+
+    // To ensure the message card can vary in size depending on the message size,
+    // ... the card will expand horizontally at first for shorter messages
+    // ... then expand vertically for longer messages
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(top = 4.dp, bottom = 4.dp)
+    ){
+        if (isSenderMe){
+            Spacer(
+                modifier = Modifier
+                    .weight(messageWidthWeight)
+            )
+        }
+        Row(
+            horizontalArrangement = rowArrangement,
+            modifier = Modifier
+                .weight(1 - messageWidthWeight)
+        ){
+            Column(
+                horizontalAlignment = columnAlignment,
+                modifier = Modifier
+            ){
+                Card(
+                    modifier = Modifier
+                ){
+                    Text(
+                        text = message,
+                        style = MaterialTheme.typography.bodyLarge,
+                        softWrap = true,
+                        overflow = TextOverflow.Clip,
+                        modifier = Modifier
+                            .padding(8.dp)
+                    )
+                }
+                Text(
+                    text = "Sent by someone",
+                    style = MaterialTheme.typography.labelSmall
+                )
+            }
+        }
+        if (!isSenderMe){
+            Spacer(
+                modifier = Modifier
+                    .weight(messageWidthWeight)
+            )
+        }
+    }
+}
+// endregion Functions
+
+
+// region PreviewFunctions
+@Preview(showBackground = true)
+@Composable
+fun MessagesScreenLayoutPreview(){
+    MyApplicationTheme {
+        val repository = MockRepository()
+        val toolbarViewModel = ToolbarViewModel(repository)
+        MessagesScreenLayout(
+            toolbarViewModel = toolbarViewModel,
+//            navController = rememberNavController()
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MessagesTopBarPreview(){
+    MyApplicationTheme {
+        MessagesTopBar()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MessagesBodyContentPreview(){
+    MyApplicationTheme {
+        MessagesBodyContent()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SingleMessagePreview(){
+    MyApplicationTheme {
+        SingleMessage(
+            isSenderMe = true,
+            message = stringResource(id = R.string.user_profile_test_string)
+        )
+    }
+}
+// endregion PreviewFunctions
