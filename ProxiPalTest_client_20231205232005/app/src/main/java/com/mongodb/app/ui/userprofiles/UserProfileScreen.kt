7c69709fbab348fc7eed519156ba19072cc85407
chetpls <80933465+chetpls@users.noreply.github.com>
2024-04-06 01:45:44 -0700

    updated delete account ui

--- ProxiPalTest_client_20231205232005/app/src/main/java/com/mongodb/app/ui/userprofiles/UserProfileScreen.kt
+++ ProxiPalTest_client_20231205232005/app/src/main/java/com/mongodb/app/ui/userprofiles/UserProfileScreen.kt
@@ -23,13 +23,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.AlertDialogDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
@@ -42,10 +46,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
@@ -512,33 +519,114 @@ fun UserProfileEditButtons(
             )
         }
     }
+    DeleteConfirmationDialog(
+        showDeleteConfirmationDialog = showDeleteConfirmationDialog,
+        onDeleteAccountConfirmed =  onDeleteAccountConfirmed,
+        onDismissRequest = { showDeleteConfirmationDialog= false },
+        toolbarViewModel = toolbarViewModel
+    )
 
+//    if (showDeleteConfirmationDialog) {
+//        AlertDialog(onDismissRequest = { showDeleteConfirmationDialog = false}) {
+//            Surface(
+//                modifier=Modifier.fillMaxWidth(),
+////                tonalElevation = AlertDialogDefaults.TonalElevation,
+//                color = Color.White,
+//                shape = RoundedCornerShape(12.dp)
+//            ) {
+//            Column(modifier=Modifier.padding(16.dp),
+//                horizontalAlignment = Alignment.CenterHorizontally,
+//                verticalArrangement =  Arrangement.spacedBy(24.dp))
+//            {
+//                Text(text = stringResource(id = R.string.are_you_sure ))
+//                Text(text = stringResource(id = R.string.delete_account_confirmation_message))
+//                Row(
+//                    modifier= Modifier.fillMaxWidth(),
+//                    verticalAlignment = Alignment.CenterVertically
+//                ){
+//                    TextButton(
+//                        onClick = {
+//                            showDeleteConfirmationDialog = false
+//                            onDeleteAccountConfirmed()
+//                            toolbarViewModel.logOut()
+//                        }, modifier = Modifier.fillMaxWidth().weight(1f)
+//                    ) {
+//                        Text(text = stringResource(id = R.string.delete_account))
+//                    }
+//                    TextButton(
+//                        onClick = { showDeleteConfirmationDialog = false },
+//                        modifier = Modifier.fillMaxWidth().weight(1f)
+//                    ) {
+//                        Text(text = stringResource(id = R.string.cancel))
+//                    }
+//                }
+//            }
+//
+//            }
+//        }
+//    }
+
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DeleteConfirmationDialog(
+    showDeleteConfirmationDialog: Boolean,
+    onDeleteAccountConfirmed: () -> Unit,
+    onDismissRequest: () -> Unit,
+    toolbarViewModel: ToolbarViewModel
+) {
     if (showDeleteConfirmationDialog) {
-        AlertDialog(
-            onDismissRequest = { showDeleteConfirmationDialog = false },
-            title = { Text(text = stringResource(id = R.string.delete_account)) },
-            text = { Text(text = stringResource(id = R.string.delete_account_confirmation_message)) },
-            confirmButton = {
-                TextButton(
-                    onClick = {
-                        showDeleteConfirmationDialog = false
-                        onDeleteAccountConfirmed()
-                        toolbarViewModel.logOut()
+        AlertDialog(onDismissRequest = onDismissRequest) {
+            Surface(
+                modifier = Modifier.fillMaxWidth(),
+                color = Color.White,
+                tonalElevation = AlertDialogDefaults.TonalElevation,
+                shadowElevation = 20.dp,
+                shape = RoundedCornerShape(12.dp)
+            ) {
+                Column(
+                    modifier = Modifier.padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(24.dp)
+                ) {
+                    Text(text = stringResource(id = R.string.are_you_sure))
+                    Text(text = stringResource(id = R.string.delete_account_confirmation_message))
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        TextButton(
+                            onClick = onDismissRequest,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(1f)
+                        ) {
+                            Text(text = stringResource(id = R.string.cancel))
+                        }
+                        TextButton(
+                            onClick = {
+                                onDismissRequest()
+                                onDeleteAccountConfirmed()
+                                toolbarViewModel.logOut()
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(1f)
+                        ) {
+                            Text(text = stringResource(id = R.string.confirm))
+                        }
+
                     }
-                ) {
-                    Text(text = stringResource(id = R.string.delete_account))
-                }
-            },
-            dismissButton = {
-                TextButton(
-                    onClick = { showDeleteConfirmationDialog = false }
-                ) {
-                    Text(text = stringResource(id = R.string.cancel))
                 }
             }
-        )
+        }
     }
 }
+
+
+
 
 
 
