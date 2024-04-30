6a96751100571236e4be97ee45d012ae257a7fa8
chetpls <80933465+chetpls@users.noreply.github.com>
2024-04-29 17:14:18 -0700

    fixed announcement input box

--- ProxiPalTest_client_20231205232005/app/src/main/java/com/mongodb/app/ui/events/EventDetails.kt
+++ ProxiPalTest_client_20231205232005/app/src/main/java/com/mongodb/app/ui/events/EventDetails.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -330,7 +331,9 @@ fun newAnnouncementDialog(
             ) {
                 OutlinedTextField(value = announcement,
                     onValueChange = {announcement = it},
-                    modifier  = Modifier.fillMaxWidth(),
+                    modifier  = Modifier
+                        .fillMaxWidth()
+                        .heightIn(min = 150.dp),
                     label = {Text("Announcement")},
                     maxLines = 5)
             }
