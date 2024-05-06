fc09edd93fe742c27fdb929736cc330ee33d6fd6
kevink02 <kevinkubota0@outlook.com>
2024-05-05 20:39:27 -0700

    Revert "Updated UI test"
    This reverts commit e151518ab99c0d6ed3ee90476a039ed468e38910.

--- ProxiPalTest_client_20231205232005/app/src/main/java/com/mongodb/app/ui/blocking_censoring/BlockUsersUI.kt
+++ ProxiPalTest_client_20231205232005/app/src/main/java/com/mongodb/app/ui/blocking_censoring/BlockUsersUI.kt
@@ -25,7 +25,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.mongodb.app.R
@@ -93,7 +92,6 @@ fun BlockingContextualMenu(
                 .clickable {
                     isContextualMenuOpen = true
                 }
-                .testTag("blockingContextualMenu")
         )
 
         DropdownMenu(
