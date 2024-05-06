e151518ab99c0d6ed3ee90476a039ed468e38910
kevink02 <kevinkubota0@outlook.com>
2024-05-05 19:38:19 -0700

    Updated UI test

--- ProxiPalTest_client_20231205232005/app/src/main/java/com/mongodb/app/ui/blocking_censoring/BlockUsersUI.kt
+++ ProxiPalTest_client_20231205232005/app/src/main/java/com/mongodb/app/ui/blocking_censoring/BlockUsersUI.kt
@@ -25,6 +25,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.mongodb.app.R
@@ -92,6 +93,7 @@ fun BlockingContextualMenu(
                 .clickable {
                     isContextualMenuOpen = true
                 }
+                .testTag("blockingContextualMenu")
         )
 
         DropdownMenu(
