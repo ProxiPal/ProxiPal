8acc1cfcb56b8ccb299947985be7b45eaa08e8c7
kevink02 <kevinkubota0@outlook.com>
2024-05-05 20:54:40 -0700

    Reapply "Updated UI test"
    This reverts commit fc09edd93fe742c27fdb929736cc330ee33d6fd6.

--- ProxiPalTest_client_20231205232005/app/src/androidTest/java/com/mongodb/app/StringCensoringUITests.kt
+++ ProxiPalTest_client_20231205232005/app/src/androidTest/java/com/mongodb/app/StringCensoringUITests.kt
@@ -1,35 +1,46 @@
 package com.mongodb.app
 
-import androidx.activity.ComponentActivity
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
-import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.navigation.compose.rememberNavController
-import com.mongodb.app.data.RealmSyncRepository
-import com.mongodb.app.home.HomeViewModel
-import com.mongodb.app.presentation.tasks.ToolbarViewModel
-import com.mongodb.app.presentation.userprofiles.UserProfileViewModel
+import com.mongodb.app.data.MockRepository
+import com.mongodb.app.data.SyncRepository
+import com.mongodb.app.data.blocking_censoring.MockBlockingCensoringRealm
+import com.mongodb.app.data.messages.MockConversationRepository
+import com.mongodb.app.data.messages.MockMessagesRepository
+import com.mongodb.app.presentation.blocking_censoring.BlockingViewModel
+import com.mongodb.app.presentation.blocking_censoring.CensoringViewModel
+import com.mongodb.app.presentation.messages.MessagesViewModel
+import com.mongodb.app.ui.messages.MessagesScreenLayout
 import com.mongodb.app.ui.theme.MyApplicationTheme
-import com.mongodb.app.ui.userprofiles.UserProfileLayout
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Rule
 import org.junit.Test
 
+@Deprecated(
+    message = "Currently does not work due to Realm related functions having to be implemented"
+)
 class StringCensoringUITests {
 
     @get: Rule
     val composeTestRule = createComposeRule()
 
-    private val repository = RealmSyncRepository{ _, _ -> }
-    private val owner = ComponentActivity()
-    private val homeViewModel = HomeViewModel(
-        repository
+    private val repository: SyncRepository = MockRepository()
+    private val blockingCensoringRealm = MockBlockingCensoringRealm()
+    private val messagesRepository = MockMessagesRepository()
+    private val conversationsRepository = MockConversationRepository()
+
+    private val messagesViewModel = MessagesViewModel(
+        repository, messagesRepository, conversationsRepository
     )
-    private val toolbarViewModel = ToolbarViewModel(
-        repository
+    private val usersInvolved = sortedSetOf(
+        "65e96193c6e205c32b0915cc", "6570119696faac878ad696a5"
     )
-    private val userProfileViewModel = UserProfileViewModel(
-        repository
+    private val blockingViewModel = BlockingViewModel(
+        repository, blockingCensoringRealm
+    )
+    private val censoringViewModel = CensoringViewModel(
+        repository, blockingCensoringRealm, false
     )
 
 
@@ -38,21 +49,16 @@ class StringCensoringUITests {
     fun censorStrings(){
         composeTestRule.setContent {
             MyApplicationTheme {
-//                val navController = rememberNavController()
-//                UserProfileLayout(
-//                    userProfileViewModel = userProfileViewModel,
-//                    toolbarViewModel = toolbarViewModel,
-//                    navController = navController,
-//                    homeViewModel = homeViewModel
-//                )
-                ComposeLoginActivity()
+                MessagesScreenLayout(
+                    navController = rememberNavController(),
+                    messagesViewModel = messagesViewModel,
+                    conversationUsersInvolved = usersInvolved,
+                    blockingViewModel = blockingViewModel,
+                    censoringViewModel = censoringViewModel
+                )
             }
         }
-        // Should be the same as the icon button to navigate to settings screen
-        // ... in TaskAppToolbar.kt
-        composeTestRule.onNodeWithContentDescription("settings screen icon button")
-            .performClick()
-            composeTestRule.onNodeWithContentDescription("dummy")
-            .assertExists("Settings screen icon button not found")
+        composeTestRule.onNodeWithTag("blockingContextualMenu")
+            .assertExists("Blocking contextual menu does not exist")
     }
 }
